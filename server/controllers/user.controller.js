58e1064916e4593d5887d5bda28fadcc9e3ab783
Bilelbourgou <bilelbourgou@gmail.com>
2023-12-13 18:45:33 +0100

    create update cover picture api route

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,7 +1,7 @@
 const userModel = require('../models/user.model.js');
 
 const updateUserController = (req, res) => {
-    const userId = req.params.userId; 
+    const userId = req.params.id; 
     const newData = req.body;
 
     userModel.updateUser(userId, newData, (err, result) => {
@@ -13,6 +13,20 @@ const updateUserController = (req, res) => {
     });
 };
 
+const updateUserCoverPicController = (req, res) => {
+    const userId = req.params.id; 
+    console.log(userId)
+    const newCoverPic = req.body.coverUrl;
+    userModel.updateUserCoverPic(userId, newCoverPic,(err, result) => {
+        if (err) {
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+            res.json({ message: 'Cover picture updated successfully', result });
+        }
+    })
+}
+
 module.exports = {
-    updateUserController
+    updateUserController,
+    updateUserCoverPicController
 };

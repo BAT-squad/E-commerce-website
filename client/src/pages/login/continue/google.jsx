5cdd4f2cf2a90e0ce23e3dc5048179be4349e511
Bilelbourgou <bilelbourgou@gmail.com>
2023-12-14 16:33:47 +0100

    fix the sign in with google error

--- client/src/pages/login/continue/google.jsx
+++ client/src/pages/login/continue/google.jsx
@@ -1,40 +1,70 @@
 import { FcGoogle } from "react-icons/fc";
-import  {googleProvider,auth} from "../../../firebase"
-import   {signInWithPopup} from "firebase/auth"
+import { googleProvider, auth } from "../../../firebase";
+import { signInWithPopup } from "firebase/auth";
 import axios from "axios";
 import { useState } from "react";
-import {useNavigate} from "react-router-dom"
-const Google = ()=>{
-    const [emailValue, setEmailValue] = useState("")
-    const[profilePicture,setProfilePicture]= useState("")
-    const[username,setUsername] = useState("")
-    const navigate = useNavigate()
-    const handleClick = ()=>{
-        
-        signInWithPopup(auth,googleProvider).then((data)=>{
-            setEmailValue(data.user.email)
-            
-            setUsername(data.user.displayName)
-            setProfilePicture(data.user.photoURL)
-            axios.post("http://localhost:5001/api/user/createUser",{userName: username,profilePicture,email: emailValue,password: "haha"}).then((response)=>{
-                console.log(response.data,'fffff')
-            })
-            localStorage.setItem("currentUser", JSON.stringify({
-                email: data.user.email,
-                profilePicture: data.user.photoURL,
-                username: data.user.displayName
-              }));
-              var storedData = JSON.parse(localStorage.getItem("currentUser"));
-            console.log(data.user)
-            navigate("/")
-        })
+import { useNavigate } from "react-router-dom";
 
+const Google = () => {
+  const [emailValue, setEmailValue] = useState("");
+  const [profilePicture, setProfilePicture] = useState("");
+  const [username, setUsername] = useState("");
+  const navigate = useNavigate();
+
+  const handleClick = async () => {
+    try {
+      const data = await signInWithPopup(auth, googleProvider);
+      console.log(data.user);
+
+      // Assign values here within the .then() block
+      const userEmail = data.user.email;
+      const userDisplayName = data.user.displayName;
+      const userPhotoURL = data.user.photoURL;
+
+      // Make the API call using async/await
+      const response = await axios.post("http://localhost:5001/api/user/createUser", {
+        userName: userDisplayName,
+        profilePicture: userPhotoURL,
+        email: userEmail,
+        password: "haha",
+      });
+
+      console.log(response.data, "fffff");
+
+      setEmailValue(userEmail);
+      setUsername(userDisplayName);
+      setProfilePicture(userPhotoURL);
+
+      localStorage.setItem(
+        "currentUser",
+        JSON.stringify({
+          email: userEmail,
+          profilePicture: userPhotoURL,
+          username: userDisplayName,
+        })
+      );
+
+      var storedData = JSON.parse(localStorage.getItem("currentUser"));
+      console.log(data.user);
+      navigate("/");
+    } catch (error) {
+      console.error("Error during Google sign-in:", error);
+      // Handle error as needed
     }
-    console.log(emailValue)
-    return(
-        <div>
-            <button onClick={handleClick} className=" text-[100%]  flex items-center gap-2 text-white  rounded-full px-[16%] mx-auto py-3 mt-2 border-[0.1px] whitespace-nowrap"> <FcGoogle  className="text-2xl"/> Continue With Google</button>
-        </div>
-    )
-}
+  };
+
+  console.log(emailValue);
+
+  return (
+    <div>
+      <button
+        onClick={handleClick}
+        className="text-[100%] flex items-center gap-2 text-white rounded-full px-[16%] mx-auto py-3 mt-2 border-[0.1px] whitespace-nowrap"
+      >
+        <FcGoogle className="text-2xl" /> Continue With Google
+      </button>
+    </div>
+  );
+};
+
 export default Google;
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,9 +1,21 @@
 const db = require('./index.js')
 
-
+const getUser = (email, callback) => {
+    const query = 'SELECT * FROM users WHERE email =?';
+    const values = [email];
+    db.query(query, values, (err, result) => {
+        if (err) {
+            console.error('Database error:', err);
+            callback(err, null);
+        }
+        else {
+            callback(null, result);
+        }
+    })
+}
 const updateUser = (id, newData, callback) => {
     const query = 'UPDATE users SET userName = ?, bio = ?, password = ?, email = ?, birthday = ? WHERE userID = ?';
-    const values = [newData.userName, newData.bio, newData.password, newData.email, newData.birthday,id];
+    const values = [newData.userName, newData.bio, newData.password, newData.email, newData.birthday, id];
 
     db.query(query, values, (err, result) => {
         if (err) {
@@ -15,9 +27,9 @@ const updateUser = (id, newData, callback) => {
     });
 };
 
-const updateUserCoverPic = (id,newCoverPic,callback)=>{
+const updateUserCoverPic = (id, newCoverPic, callback) => {
     const query = 'UPDATE users SET coverUrl = ? WHERE userID = ?'
-    const values = [newCoverPic,id]
+    const values = [newCoverPic, id]
     db.query(query, values, (err, result) => {
         if (err) {
             console.error('Database error:', err);
@@ -28,9 +40,9 @@ const updateUserCoverPic = (id,newCoverPic,callback)=>{
     })
 }
 
-const updateUserProfilePic = (id,newProfilePic,callback)=>{
+const updateUserProfilePic = (id, newProfilePic, callback) => {
     const query = 'UPDATE users SET profilePicture =? WHERE userID =?'
-    const values = [newProfilePic,id]
+    const values = [newProfilePic, id]
     db.query(query, values, (err, result) => {
         if (err) {
             console.error('Database error:', err);
@@ -42,13 +54,14 @@ const updateUserProfilePic = (id,newProfilePic,callback)=>{
 }
 
 module.exports = {
+    getUser,
     updateUser,
     updateUserCoverPic,
     updateUserProfilePic,
-    addUser: function(email,userName,birthday,password,coverUrl,bio,profilePicture,callback){
+    addUser: function (email, userName, birthday, password, coverUrl, bio, profilePicture, callback) {
         const sql = `insert into users (email,userName,birthday,password,coverUrl,bio,profilePicture) values(?,?,?,?,?,?,?)`
-        db.query(sql,[email,userName,birthday,password,coverUrl,bio,profilePicture],function(error,results){
-            callback(error,results)
+        db.query(sql, [email, userName, birthday, password, coverUrl, bio, profilePicture], function (error, results) {
+            callback(error, results)
         })
     }
 };
